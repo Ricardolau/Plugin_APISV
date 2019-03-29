d1ecceb3b58fff5f1d94f85e1776f669102a1be6
Ricardo Oficina <ricardolau6@gmail.com>
2019-03-29 09:33:50 +0100

    Preparo para subir Github

---
+++ apisv.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @license http://www.gnu.org/licenses GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// no direct access
+defined('_JEXEC') or die('Restricted access');
+jimport('joomla.plugin.plugin');
+
+class plgSystemPapiv extends JPlugin
+{
+
+        var $plugin = null;
+        var $plgParams = null;
+        var $time = 0;
+        protected $autoloadLanguage = true;
+
+        
+
+}
